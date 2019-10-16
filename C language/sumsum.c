9d6511d76544136242719cdaeeee1f5469806abd
Han Ji Hye <37392113+jh1023@users.noreply.github.com>
2019-10-16 14:43:48 +0900

    Create sumsum.c

---
+++ C language/sumsum.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+
+{
+	int i;
+	int sum = 0; //1~100까지 합계
+	int sum2 = 0; //1~100까지 짝수 합계
+	int sum3 = 0; //1~100까지 홀수 합계
+	
+	for (i = 0; i <= 100; i++) {
+		sum = sum + i;
+		
+	}// 1~100까지의 합 (5050)
+	printf("합계 %d 입니다.\n", sum);
+
+	for (i = 1; i < 100; i++) {
+		i++;
+		sum2 = sum2 + i;
+
+	}//짝수 합 (2550)
+	printf("합계 %d 입니다. 짝수 합(1~100).\n", sum2);
+
+
+	for (i = 0; i < 100; i++) {
+		i++;
+		sum3 = sum3 + i;
+
+	}// 홀수 합 (2500)
+	printf("합계 %d 입니다. 홀수 합(1~100)\n", sum3);
+
+	
+	return 0;
+}
