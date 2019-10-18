e32c8989a61d0de374e487805bec9eef6966c492
Han Ji Hye <37392113+jh1023@users.noreply.github.com>
2019-10-18 17:02:34 +0900

    Add files via upload

---
+++ C language/191018_gugudan.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+void gugudan(int a, int b)
+{
+
+	if (a < b)
+		for (a = 2; a < b + 1; a++)
+		{
+			int i = 1;
+			for (i = 1; i < 10; i++)
+			{
+				printf("%d x %d = %d \n", a, i, a * i);
+			}
+			printf("\n");
+		}
+	else
+		for (b = 2; b < a + 1; b++)
+		{
+			int i = 1;
+			for (i = 1; i < 10; i++)
+			{
+				printf("%d x %d = %d \n", b, i, b * i);
+			}
+			printf("\n");
+
+		}
+}
+int main(void)
+{
+	int gugu1, gugu2;
+	printf("두 개의 정수를 입력하세요 : \n ");
+
+	scanf("%d%d", &gugu1, &gugu2);
+	
+	gugudan(gugu1, gugu2);
+	
+	return 0;
+}
+
+
+
+
+
+
